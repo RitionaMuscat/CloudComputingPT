2148c930cc3b6d3fd11df843220cfe7409caef45
RitionaMuscat <57151182+RitionaMuscat@users.noreply.github.com>
2021-09-15 21:28:19 +0200

    changes

--- CloudComputingPT/Controllers/PassengerController.cs
+++ CloudComputingPT/Controllers/PassengerController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CloudComputingPT.Data;
+using CloudComputingPT.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +13,34 @@ namespace CloudComputingPT.Controllers
 {
     public class PassengerController : Controller
     {
+        private ApplicationDbContext _applicationDBContext;
+        private readonly UserManager<IdentityUser> _userManager;
+        public PassengerController(ApplicationDbContext applicationDBContext, UserManager<IdentityUser> userManager)
+        {
+            _applicationDBContext = applicationDBContext;
+            _userManager = userManager;
+        }
         // GET: PassengerController
         public ActionResult Index()
         {
-            return View();
+            BookingDetails Det = new BookingDetails();
+
+            var PassengerBookings = (from a in _applicationDBContext.bookingDetails
+                                    select a).ToList();
+
+            foreach (var item in PassengerBookings)
+            {
+                Det.Id = item.Id;
+                Det.passengerId = item.passengerId;
+                Det.residingAdress = item.residingAdress;
+                Det.isBookingConfirmed = item.isBookingConfirmed;
+                Det.destinationAddress = item.destinationAddress;
+            }
+           
+            if (Det.Id != null)
+                return View(Det);
+            else
+                return View();
         }
 
         // GET: PassengerController/Details/5
@@ -30,10 +58,18 @@ namespace CloudComputingPT.Controllers
         // POST: PassengerController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public ActionResult Create(/*IFormCollection collection*/BookingDetails details)
         {
             try
             {
+                if (User.Identity.IsAuthenticated)
+                {
+                    var current_User = _userManager.GetUserId(User);
+                    details.passengerId = new Guid(current_User);
+                  _applicationDBContext.bookingDetails.Add(details);
+                    _applicationDBContext.SaveChanges();
+                    
+                }
                 return RedirectToAction(nameof(Index));
             }
             catch
