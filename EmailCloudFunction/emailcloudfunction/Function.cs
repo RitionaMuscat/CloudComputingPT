e5c4bdd34d03b638e6f2a66e851ed1b329b01dc8
RitionaMuscat <57151182+RitionaMuscat@users.noreply.github.com>
2021-10-02 15:34:25 +0200

    changes

--- EmailCloudFunction/emailcloudfunction/Function.cs
+++ EmailCloudFunction/emailcloudfunction/Function.cs
@@ -2,15 +2,14 @@ using CloudNative.CloudEvents;
 using Google.Cloud.Functions.Framework;
 using Google.Events.Protobuf.Cloud.PubSub.V1;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace emailcloudfunction
 {
-       private readonly ILogger _logger;
-
-        public Function(ILogger<Function> logger) =>
-            _logger = logger;
+    
+  
     /// <summary>
     /// A function that can be triggered in responses to changes in Google Cloud Storage.
     /// The type argument (StorageObjectData in this case) determines how the event payload is deserialized.
@@ -20,22 +19,27 @@ namespace emailcloudfunction
     /// </summary>
     public class Function : ICloudEventFunction<MessagePublishedData>
     {
-        /// <summary>
-        /// Logic for your function goes here. Note that a CloudEvent function just consumes an event;
-        /// it doesn't provide any response.
-        /// </summary>
-        /// <param name="cloudEvent">The CloudEvent your function should consume.</param>
-        /// <param name="data">The deserialized data within the CloudEvent.</param>
-        /// <param name="cancellationToken">A cancellation token that is notified if the request is aborted.</param>
-        /// <returns>A task representing the asynchronous operation.</returns>
-        public Task HandleAsync(CloudEvent cloudEvent, StorageObjectData data, CancellationToken cancellationToken)
+        private readonly ILogger _logger;
+
+        public Function(ILogger<Function> logger) =>
+            _logger = logger;
+
+        public Task HandleAsync(CloudEvent cloudEvent, MessagePublishedData data, CancellationToken cancellationToken)
         {
-             
             string nameFromMessage = data.Message?.TextData;
-            string name = string.IsNullOrEmpty(nameFromMessage) ? "world" : nameFromMessage;
-            _logger.LogInformation("Hello {name}", name);
+
+            dynamic deserializeObject = JsonConvert.DeserializeObject(nameFromMessage);
+
+          
+            string to = deserializeObject.To.ToString();
+            string body= deserializeObject.From.ToString();
+
+
+          //  string name = string.IsNullOrEmpty(nameFromMessage) ? "world" : nameFromMessage;
+            _logger.LogInformation("To {to}", to);
+            _logger.LogInformation("body {body}", body);
+
             return Task.CompletedTask;
         }
-        
     }
 }
